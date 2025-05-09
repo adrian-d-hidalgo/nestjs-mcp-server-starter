edd5477ecc9d064dba873eb20d8d848e841eb262
Adrián Hidalgo <adrian.hidalgo@mailfusion.me>
2025-05-09 16:58:16 -0600

    refactor: Remove unused AppController and AppService (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [CoreModule, HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
