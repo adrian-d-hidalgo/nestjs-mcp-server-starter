dc59dfe0520fd734b078d97cfdb07eab225c17ee
Adrian Darío Hidalgo Flores <adrian.hidalgo@mailfusion.me>
2025-05-09 14:29:26 -0600

    feat: Basic @nestjs-mcp/server implementation

---
+++ src/core/core.module.ts
@@ -0,0 +1,17 @@
+import { McpModule } from '@nestjs-mcp/server';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [
+    McpModule.forRoot({
+      // TODO: Replace with your custom MCP server name (must be unique)
+      name: 'nestjs-mcp-starter',
+      // TODO: Replace with your server version (e.g., '1.0.0')
+      version: '1.0.0',
+      // TODO: Add instructions that explain to the LLM how to use this server's tools and endpoints
+      instructions:
+        'This is nestjs mcp server starter to understand the MCP protocol using the NestJS framework',
+    }),
+  ],
+})
+export class CoreModule {}
